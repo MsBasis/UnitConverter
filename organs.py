46af6d402ce93c2c0dc08ebd5e1514ad4d3046ee
Mati G <matikgawin@gmail.com>
2025-04-28 14:50:18 +0200

    ADD added window for the app

--- organs.py
+++ organs.py
@@ -1,1 +1,26 @@
 #GUI File
+import tkinter as tk
+
+root = tk.Tk()
+
+# – – – – – – – – – – – – – – DIMENSIONS – – – – – – – – – – – – – – – – –
+root.title("Unit Converter")
+root.configure(background="#041775")
+root.minsize(500,500)
+root.maxsize(500,500)
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
