08945a6e9bea3afdf817cb4af813a820d61e790f
Mati G <matikgawin@gmail.com>
2025-04-28 14:44:54 +0200

    ADD

--- Converter/length.py
+++ Converter/length.py
@@ -5,3 +5,16 @@
 #MILES
 #FOOT
 #INCHES
+
+NormalPeopleUnits = {
+    "centimeters" : 100,
+    "meters" : 1,
+    "kilometers" : 0.001,
+}
+
+def metersConverter(unit,x):
+    meter = unit * NormalPeopleUnits[f"{x}"]
+    print(meter)
+
+metersConverter(10,"kilometers")
+
