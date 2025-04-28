9b0bf5fbc3884e86ead4179541557af1e0a43ca9
Mati G <matikgawin@gmail.com>
2025-04-28 15:11:27 +0200

    ADD added constants file so i can handle conversions better

---
+++ Converter/constants.py
@@ -0,0 +1,21 @@
+#UnitsDict
+
+LenghtUnits = {
+    'm' : {
+        "centimeter" : 100,
+        "meter" : 1,
+        "kilometer" : 0.001,
+        "mile" : 0.000621371192,
+        "feet" : 0.000621371192,
+        "inches" : 39.3700787
+    },
+    'km' : {
+        "centimeter" : 100000,
+        "meter" : 1000,
+        "kilometer" : 1,
+        "mile" : 0.621371192,
+        "feet" : 3280.8399,
+        "inches" : 39370.0787
+    }
+        
+}
