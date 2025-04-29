8d34148a37b12b240506a338db59735f696a91ca
unknown <matikgawin@gmail.com>
2025-04-29 23:30:17 +0200

    ADD

--- Converter/constants.py
+++ Converter/constants.py
@@ -16,6 +16,38 @@ LenghtUnits = {
         "mile" : 0.621371192,
         "feet" : 3280.8399,
         "inches" : 39370.0787
+    },
+    'cm' : {
+        "centimeter" : 1,
+        "meter" : 0.01,
+        "kilometer" : 0.00001,
+        "mile" : 2,
+        "feet" : 2,
+        "inches" : 2
+    },
+    'mile' : {
+        "centimeter" : 2,
+        "meter" : 2,
+        "kilometer" : 2,
+        "mile" : 1,
+        "feet" : 2,
+        "inches" : 2
+    },
+    'feet' : {
+        "centimeter" : 2,
+        "meter" : 2,
+        "kilometer" : 2,
+        "mile" : 2,
+        "feet" : 1,
+        "inches" : 2
+    },
+    'inches' : {
+        "centimeter" : 2,
+        "meter" : 2,
+        "kilometer" : 2,
+        "mile" : 2,
+        "feet" : 2,
+        "inches" : 1
     }
         
 }
